857c69d41b6eb246a149858eb743cf1da89d4130
nikz7 <71370024+nikz7@users.noreply.github.com>
2021-05-01 22:28:27 -0500

    Function to drop null values and return a subset.

--- vaers.py
+++ vaers.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-def create_dataframe(filename):
+def create_dataframe(filename:str):
     """Takes the filename as a parameter and loads the data and returns its DataFrame.
 
     param filename: Name of the file for which the DataFrame will be created.
@@ -12,6 +12,13 @@ def create_dataframe(filename):
     df = pd.read_csv(filename, engine='python')
 
     return df
+
+
+def drop_null_values(dataframe, subset):
+    subset_list = [subset]
+    dataframe = dataframe.dropna(subset=subset_list)
+    return dataframe
+
 
 
 if __name__ == "__main__":
@@ -26,12 +33,13 @@ if __name__ == "__main__":
 
     # DROP LOCATIONS WHICH ARE NOT A US STATE
     vaccinations_per_state = create_dataframe("us_state_vaccinations.csv")
-
     remove_locations = vaccinations_per_state['location'].isin(
         ['Bureau of Prisons', 'Dept of Defense', 'Long Term Care', 'United States', 'Indian Health Svc'])
     vaccinations_per_state_v1 = vaccinations_per_state[~remove_locations]
 
     # DROP ROWS WITH NAN VALUES IN THE TOTAL VACCINATIONS COLUMN
-    vaccinations_per_state_v2 = vaccinations_per_state_v1.dropna(subset=['total_vaccinations'])
-
+    #vaccinations_per_state_v2 = vaccinations_per_state_v1.dropna(subset=['total_vaccinations'])
+    vaccinations_per_state_v2 = drop_null_values(vaccinations_per_state_v1, 'total_vaccinations')
+    #print(vaccinations_per_state_v1.location.unique())
     print(vaccinations_per_state_v2['total_vaccinations'])
+
