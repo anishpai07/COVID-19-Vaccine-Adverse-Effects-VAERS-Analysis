34bc1839bea1c196f133dad678aae283ded4540a
Vedant Desai <77986480+vedantdesai28@users.noreply.github.com>
2021-05-01 20:09:30 -0500

    Vedant: Vaers Data set Load

--- vaers.py
+++ vaers.py
@@ -16,14 +16,22 @@ def create_dataframe(filename):
 
 if __name__ == "__main__":
 
+    # LOAD ALL VAERS DATASETS.
+    vaers_data = create_dataframe("2021VAERSData.csv")
+    vaers_symptoms = create_dataframe("2021VAERSSYMPTOMS.csv")
+    vaers_vax = create_dataframe("2021VAERSVAX.csv")
+    #print(vaers_vax)
+
     # VACCINATIONS PER STATE DATA CLEANING AND PREPROCESSING.
 
     # DROP LOCATIONS WHICH ARE NOT A US STATE
     vaccinations_per_state = create_dataframe("us_state_vaccinations.csv")
-    remove_locations = vaccinations_per_state['location'].isin(['Bureau of Prisons', 'Dept of Defense', 'Long Term Care', 'United States', 'Indian Health Svc'])
+
+    remove_locations = vaccinations_per_state['location'].isin(
+        ['Bureau of Prisons', 'Dept of Defense', 'Long Term Care', 'United States', 'Indian Health Svc'])
     vaccinations_per_state_v1 = vaccinations_per_state[~remove_locations]
 
     # DROP ROWS WITH NAN VALUES IN THE TOTAL VACCINATIONS COLUMN
     vaccinations_per_state_v2 = vaccinations_per_state_v1.dropna(subset=['total_vaccinations'])
-    #print(vaccinations_per_state_v1.location.unique())
+
     print(vaccinations_per_state_v2['total_vaccinations'])
